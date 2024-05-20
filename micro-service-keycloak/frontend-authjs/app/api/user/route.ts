715c140770863b943dc04f03e52fa41988db110e
anusikh <anusikh2001@gmail.com>
2024-05-21 02:10:25 +0530

    basic linting

--- micro-service-keycloak/frontend-authjs/app/api/user/route.ts
+++ micro-service-keycloak/frontend-authjs/app/api/user/route.ts
@@ -1,6 +1,5 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]/route";
-import { getToken } from "next-auth/jwt";
 import { NextRequest } from "next/server";
 
 export async function GET(req: NextRequest, res: Response) {
